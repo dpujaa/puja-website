6b46d297b68970b6499844cb94b8c270c2d3d6aa
dpujaa <dpujaa@gmail.com>
2024-02-12 18:36:46 -0800

    new webpages + routing

---
+++ src/components/aboutMe.js
@@ -0,0 +1,12 @@
+import React from "react"
+import '../App.css'
+
+
+export const AboutMe = () => {
+    return(
+        <div className='AboutMe'>
+            <h1>This is all about me</h1>
+            <p>My name is Puja</p>
+        </div>
+    )
+}
---
+++ src/components/error.js
@@ -0,0 +1,12 @@
+import React from "react"
+import '../App.css'
+
+
+export const Error = () => {
+    return(
+        <div className='Error'>
+            <h1 style={{color: 'red'}}>ERROR</h1>
+            <p>You are wrong</p>
+        </div>
+    )
+}
